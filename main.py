adfae48c7b914a44458da544809770f499afc654
Chai <83523587+CPScript@users.noreply.github.com>
2022-12-14 11:50:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,42 @@
+import time
 import os
-os.system('ls -l')
+
+choice = input("")
+
+if choice == "yes":
+    time.sleep(0.05)
+    os.system(delet)
+    os.system('pip3 install psutil')
+    os.system('pip3 install gputil')
+    os.system('pip3 install tabulate')
+    os.system('pip3 install cryptography')
+    os.system('pip install AppOpener')
+    time.sleep(2)
+    print("Would you like to see more Tools, Viruses, and Hacks?")
+    import os
+
+    choice = input("")
+
+    if choice == "yes":
+        time.sleep(0.05)
+        os.system(delet)
+        print("OK")
+        time.sleep(2)
+        print("https://CPScript/Tools")
+        time.sleep(2)
+        print("Running script in 10 Secounds")
+        tims.sleep(10)
+        call(["python", "LMAO.py"])
+
+    if choice == "no":
+        time.sleep(0.05)
+        os.system(delet)
+        call(["python", "fail.py"])
+
+      
+
+if choice == "no":
+    time.sleep(0.05)
+    os.system(delet)
+    call(["python", "fail.py"])
+    
