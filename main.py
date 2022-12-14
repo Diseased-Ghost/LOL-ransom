7a2d551cf2a6b8a5fa9dead081ddd6b91c779062
Chai <83523587+CPScript@users.noreply.github.com>
2022-12-14 11:57:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ choice = input("")
 
 if choice == "yes":
     time.sleep(0.05)
-    os.system(delet)
     os.system('pip3 install psutil')
     os.system('pip3 install gputil')
     os.system('pip3 install tabulate')
@@ -22,24 +21,23 @@ if choice == "yes":
 
     if choice == "yes":
         time.sleep(0.05)
-        os.system(delet)
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
         print("OK")
         time.sleep(2)
         print("https://CPScript/Tools")
         time.sleep(2)
         print("Running script in 10 Secounds")
-        tims.sleep(10)
+        time.sleep(10)
         call(["python", "LMAO.py"])
 
     if choice == "no":
-        time.sleep(0.05)
-        os.system(delet)
         call(["python", "fail.py"])
 
       
 
 if choice == "no":
-    time.sleep(0.05)
-    os.system(delet)
     call(["python", "fail.py"])
     
